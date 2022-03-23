17ba5b97cd96b52a93188b4e8dd612cac9c1fe79
“ArafathvBaig” <arafathbaig1997@gmail.com>
2022-03-23 10:28:58 +0530

    Added: Welcome message to the master

---
+++ src/Address_Book.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Author -> Arafath Baig
+ * PHP Version -> 8.0.9
+ * Class for Address Book Computation Problem
+ */
+class Address_Book
+{
+    function welcomeMessage()
+    {
+        echo "Welcome to Address Book Computation Problem\n";
+    }
+}
+$addressBook = new Address_Book();
+$addressBook->welcomeMessage();
